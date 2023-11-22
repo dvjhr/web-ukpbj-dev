65493076a1f7469c2e99dc91be2e2400a6c0ac02
dvjhr <2dava.aditya21@gmail.com>
2023-11-22 23:11:04 +0700

    fast api adaptation to FE

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-
+# from fastapi.templating import Jinja2Templates
+from starlette.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 
 @app.get("/")
 async def home(request: Request):
     context = {"request": request}
-    return templates.TemplateResponse("base.html", context)
+    return templates.TemplateResponse("beranda.html", context)
+
+@app.get("/{page_name}")
+def any_page(request: Request, page_name: str):
+    return templates.TemplateResponse(f"{page_name}.html", context={"request":request})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
