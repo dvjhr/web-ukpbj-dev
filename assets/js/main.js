3474aa81aab24fdafbbc62389c2c5f7fa74570c3
sharira <sharirasaniane95@gmail.com>
2023-11-30 11:55:26 +0700

    akun dan login

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,23 @@ hamburgerBtn.addEventListener('click', () => {
     const sidebar = document.querySelector('#sidebar-wrapper')
     sidebar.classList.toggle('toggled')
 })
+
+// AKUN
+function changeProfilePic() {
+    const input = document.getElementById('profilePicUpload');
+    const img = document.getElementById('profilePic');
+    const file = input.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        img.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  // Tambahkan event listener untuk memanggil changeProfilePic saat input file berubah
+  document.getElementById('profilePicUpload').addEventListener('change', changeProfilePic);
+  
+  
